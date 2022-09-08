425ecac588f1819c8b9d2af1378fbde0673b12d6
Ian-Barton <bobbarbid.1@gmail.com>
2022-09-08 10:36:09 -0500

    Table set up
    Set up the layout and look of the website

--- js/site.js
+++ js/site.js
@@ -1,3 +1,13 @@
-function HelloWorld() {
-    alert("Hello World");
+function getValues() {
+    let loan = document.getElementById("loan").value;
+    let term = document.getElementById("term").value;
+    let rate = document.getElementById("rate").value;
+}
+
+function calculate() {
+
+}
+
+function display() {
+
 }
